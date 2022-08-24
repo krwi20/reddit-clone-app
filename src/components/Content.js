3596d07149d96fc119964b5ec45462566e6fd140
krwi20 <kristoffwittwer@icloud.com>
2022-08-24 22:11:38 +0100

    began comment functionality + styling changes

--- src/components/Content.js
+++ src/components/Content.js
@@ -15,6 +15,17 @@ function Content() {
   }, [dispatch]);
 
   console.log(posts);
+  console.log(posts.comments);
+
+  function toggleComments(a) {
+    var comments = document.getElementById(a);
+
+    if(comments.style.display === "none") {
+      comments.style.display = "flex";
+    } else {
+      comments.style.display = "none";
+    }
+  }
 
   return (
     <>
@@ -23,15 +34,23 @@ function Content() {
           <div className="content__container">
             <div className="content__details">
               <h1>{reddit.title}</h1>
-              <div className="content__image__container">
-                {reddit.is_video === true | reddit.is_gallery === true ? <img className="content__image" src={reddit.thumbnail} /> : <img className="content__image" src={reddit.url}/>}
-              </div>
-              <div className="content__info">
-               <p>Author: u/{reddit.author}</p>
-               {/* https://momentjs.com/docs/#/use-it/ */}
-               <p>{moment.unix(reddit.created_utc).fromNow()}</p>
-               <p>Comments</p>
-              </div>
+                <div className="content__image__container">
+                {/* If the url does not include a jpg - if the post isnt an image - use a reddit logo as a placeholder */}
+                {/* If image is clicked takes you to the actual reddit post */}
+                {(reddit.url).includes("jpg")  ? <a href={`https://www.reddit.com${reddit.permalink}`} target="_blank"><img className="content__image" 
+                src={reddit.url} /></a> : <a href={`https://www.reddit.com${reddit.permalink}`} target="_blank"><img className="content__image" src="http://t1.gstatic.com/images?q=tbn:ANd9GcThsotATP9ktYH_-oqNK6lYSI2USCxC-9nhbqScnKqvWFyxmL64"/></a>}
+                </div>
+                <div className="content__info">
+                  <p>Author: u/{reddit.author}</p>
+                  {/* https://momentjs.com/docs/#/use-it/ */}
+                  <p>{moment.unix(reddit.created_utc).fromNow()}</p>
+                  <p onClick={() => toggleComments(reddit.id)}>Comments</p>
+                </div>
+                <div className="content__comment__container">
+                  <div id={reddit.id} style={{display: "none"}}>
+                    <p>{reddit.title}</p>
+                  </div>
+                </div>
             </div>
           </div>
         </div>
